5b2eeb4d854be3716338dd501bd3abe026460080
Alex Merenda <alexm@moove.ai>
2020-06-18 19:16:19 -0400

    removes ssh, http, and https allow tags

--- modules/fabric-net-firewall/variables.tf
+++ modules/fabric-net-firewall/variables.tf
@@ -53,17 +53,14 @@ variable "admin_ranges" {
 
 variable "ssh_source_ranges" {
   description = "List of IP CIDR ranges for tag-based SSH rule, defaults to 0.0.0.0/0."
-  default     = ["0.0.0.0/0"]
 }
 
 variable "http_source_ranges" {
   description = "List of IP CIDR ranges for tag-based HTTP rule, defaults to 0.0.0.0/0."
-  default     = ["0.0.0.0/0"]
 }
 
 variable "https_source_ranges" {
   description = "List of IP CIDR ranges for tag-based HTTPS rule, defaults to 0.0.0.0/0."
-  default     = ["0.0.0.0/0"]
 }
 
 variable "custom_rules" {
